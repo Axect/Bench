55429fd79770a2ab5cb2d13b0d81269016956442
axect <edeftg@gmail.com>
2018-10-31 15:10:18 +0900

    Benchmark Plot

--- data/figure/plot.R
+++ data/figure/plot.R
@@ -1,7 +1,18 @@
 library("ggpubr")
 
-a <- read.csv("inv.csv")
-lib <- c("Peroxide(Rust)", "R", "Julia", "DNumeric(D)", "Numpy(Python)")
-a <- cbind(a, lib)
+dat_inv <- read.csv("../inv.csv")
+dat_det <- read.csv("../det.csv")
+dat_add <- read.csv("../add.csv")
+dat_mul <- read.csv("../mul.csv")
 
-ggline(a, "lib", "mean", type='l', ylab="time(s)") + yscale("log10", .format=TRUE)
+dat <- rbind(dat_add, dat_mul, dat_det, dat_inv)
+
+lib <- c("Peroxide(Rust)", "DNumeric(D)", "R", "Numpy(Python)", "Julia")
+Library <- rep(lib, 4)
+
+work <- c(rep("add", 5), rep("mul", 5), rep("det", 5), rep("inv", 5))
+
+df <- cbind(dat, Library, work)
+
+g <- ggline(df, "work", "mean", type='l', color="Library", ylab="time(s)", title="Benchmark for matrix operations")
+g + yscale("log10", .format=TRUE)
